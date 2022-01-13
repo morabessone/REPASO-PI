563dcbd1c6406d9f0cdb49b984a590cbbb85c7be
Hernán García <84438812+hernancur@users.noreply.github.com>
2022-01-13 13:30:22 -0300

    redux y home

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
-const server = require('./src/app.js')
-const { getEpisodes } = require('./src/controllers/episodeControllers.js')
-const { conn } = require('./src/db.js')
+const server = require("./src/app.js");
+const { getEpisodes } = require("./src/controllers/episodeControllers.js");
+const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 
 // Para la precarga cuando se levanta el server, ejecutar la funcion getEpisodes(). Al ser una peticion vamos a usar async await.
 
 conn.sync({ force: true }).then(() => {
+  getEpisodes();
   server.listen(3001, () => {
-    console.log('Listening at 3001') // eslint-disable-line no-console
-  })
-})
+    console.log("Listening at 3001"); // eslint-disable-line no-console
+  });
+});
--- api/src/controllers/episodeControllers.js
+++ api/src/controllers/episodeControllers.js
@@ -1,30 +1,29 @@
-const {Episode} = require("../db.js");
+const { Episode } = require("../db.js");
 const axios = require("axios");
 
 const getEpisodes = async (req, res) => {
-    try {
-        let episodios = await axios.get("https://rickandmortyapi.com/api/episode")
-        let response = episodios.data.results?.map((e) => {
-            return {
-                id: e.id,
-                name: e.name,
-            }
-        })
-        response.forEach(async (e) => {
-            await Episode.findOrCreate({
-                where: {
-                    id: e.id,
-                    name: e.name
-                }
-            })
-        })
-        return res.send(response)
-
-    } catch(e) {
-        console.error(e)
-    }
-}
+  try {
+    let episodios = await axios.get("https://rickandmortyapi.com/api/episode");
+    let response = episodios.data.results?.map((e) => {
+      return {
+        id: e.id,
+        name: e.name,
+      };
+    });
+    response.forEach(async (e) => {
+      await Episode.findOrCreate({
+        where: {
+          id: e.id,
+          name: e.name,
+        },
+      });
+    });
+    // return res.send(response)
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 module.exports = {
-    getEpisodes
-}
+  getEpisodes,
+};
