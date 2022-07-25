f89af15b98e68bde64129364418aa3c899b3fe7e
Mora Bessone <79546017+morabessone@users.noreply.github.com>
2022-07-25 13:17:07 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const { conn } = require("./src/db.js");
 // Para la precarga cuando se levanta el server, ejecutar la funcion getEpisodes(). Al ser una peticion vamos a usar async await.
 
 conn.sync({ force: true }).then(() => {
-  getEpisodes();
   server.listen(3001, () => {
     console.log("Listening at 3001"); // eslint-disable-line no-console
   });
